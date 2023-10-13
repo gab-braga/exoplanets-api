fe7981693198f38cf9f0b268fec4e2d092c7f99a
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-10-12 23:31:42 -0300

    exoplanets api

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,8 +4,50 @@ const prisma = new PrismaClient();
 import { Router } from "express";
 export const apiRouter = Router();
 
+apiRouter.get("/", async (req, res) => {
+  res
+    .status(200)
+    .json({
+      message:
+        "Bem-vindo! Para iniciar acesse: https://github.com/f-gabriel-braga/exoplanets-api",
+    });
+});
+
 apiRouter.get("/exoplanets", async (req, res) => {
+  try {
     const limit = parseInt(req.query.limit) || 60;
-    const exoplanets = await prisma.exoplanet.findMany({take: limit});
+    const exoplanets = await prisma.exoplanet.findMany({ take: limit });
     res.status(200).json(exoplanets);
+  } catch (err) {
+    return res.status(500).json({ message: "Erro interno no servidor." });
+  }
+});
+
+apiRouter.get("/exoplanets/search/:search", async (req, res) => {
+  try {
+    const { search } = req.params;
+    const limit = parseInt(req.query.limit) || 60;
+    const exoplanets = await prisma.exoplanet.findMany({
+      where: { pl_name: { contains: search } },
+      take: limit,
+    });
+    res.status(200).json(exoplanets);
+  } catch (err) {
+    return res.status(500).json({ message: "Erro interno no servidor." });
+  }
+});
+
+apiRouter.get("/exoplanets/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({ message: "O id deve ser um número." });
+    }
+    const exoplanets = await prisma.exoplanet.findFirst({
+      where: { id },
+    });
+    return res.status(200).json(exoplanets);
+  } catch (err) {
+    return res.status(500).json({ message: "Erro interno no servidor." });
+  }
 });
--- src/scripts/createData.js
+++ src/scripts/createData.js
@@ -3,16 +3,16 @@ const prisma = new PrismaClient();
 
 // import fs from "fs";
 // (() => {
-//     fs.readFile("csvjson.json", "utf8", (err, data) => {
-//         if(err) return;
+//   fs.readFile("csvjson.json", "utf8", (err, data) => {
+//     if (err) return;
 
-//         const vetor = JSON.parse(data);
-//         (async () => {
-//             for(let item of vetor) {
-//                 if(item.st_age == "") delete item.st_age;
-//                 else item.st_age = String(item.st_age)
-//                 await prisma.exoplanet.create({data: item})
-//             }
-//         })();
-//     })
-// })()
+//     const vetor = JSON.parse(data);
+//     (async () => {
+//       for (let item of vetor) {
+//         if (item.st_age == "") delete item.st_age;
+//         else item.st_age = String(item.st_age);
+//         await prisma.exoplanet.create({ data: item });
+//       }
+//     })();
+//   });
+// })();
